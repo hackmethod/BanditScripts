c6b86d3b533d1fc1129de65dc3052399756a2aa9
incidrthreat <incidrthreat@gmail.com>
2021-02-14 19:17:32 -0600

    Updated

--- Bandit/Level 25/Bandit_lvl25.py
+++ Bandit/Level 25/Bandit_lvl25.py
@@ -16,7 +16,7 @@ s.connect(('localhost', 30002))
 s.recv(1024)
 while True:
         print('[+] Sending Pin: ' + str(pin))
-        s.sendall(password + str(pin)+ '\n')
+        s.sendall(password + " " + str(pin)+ '\n')
         data = s.recv(1024)
         print(data)
         pin += 1
